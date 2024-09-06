5405f3dcdd3292122d097de98c56ef5c8202a914
brigido <jv_brigido@hotmail.com>
2024-09-06 10:55:16 -0400

    feat: Implement the UserEntity

---
+++ src/main/java/dev/brigido/entity/UserEntity.java
@@ -0,0 +1,18 @@
+package dev.brigido.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name= "tb_users")
+
+public class UserEntity extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public UUID userID;
+    public String username;
+
+
+}
